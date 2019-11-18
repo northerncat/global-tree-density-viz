34a0bb34edd1cfa99b3fdf537f6e8f0a89514a06
Brad Huang <brad820309@gmail.com>
2019-11-17 17:57:33 -0800

    Add basemap to the globe
    1. Integrate three-globe and used image texture for the globe.
    2. Fixed bug with the latitude calculation.

--- src/components/visualizations/GlobeViz.jsx
+++ src/components/visualizations/GlobeViz.jsx
@@ -1,20 +1,20 @@
-import React, {
-    useEffect
-} from 'react';
+import React, { useEffect } from 'react';
 
 import * as THREE from 'three';
+import ThreeGlobe from 'three-globe';
 
 import * as GeoTIFF from 'geotiff';
 
 import OrbitControls from '../../three-examples/OrbitControls';
 
+const GLOBE_RADIUS = 6.0;
 
 function GlobeViz() {
 
     function sphericalCoordsToVector3(radius, phi, theta) {
         return new THREE.Vector3(
             Math.sin(phi) * Math.cos(theta),
-            -Math.sin(theta),
+            Math.sin(theta),
             Math.cos(phi) * Math.cos(theta)
         ).multiplyScalar(radius);
     }
@@ -25,11 +25,19 @@ function GlobeViz() {
         return { minLongitude, minLatitude, maxLongitude, maxLatitude };
     }
 
+    function createOrbitControls(camera, domElement) {
+        const orbitControls = new OrbitControls(camera, domElement);
+        orbitControls.enablePan = false;
+        orbitControls.minDistance = GLOBE_RADIUS * 1.2;
+        orbitControls.maxDistance = GLOBE_RADIUS * 3;
+        return orbitControls;
+    }
+
     function loadLights(scene) {
         const pointLight = new THREE.PointLight(0x404040, 1, 100);
-        pointLight.position.set(0, 15, 0);
+        pointLight.position.set(0, GLOBE_RADIUS * 2.5, 0);
         const pointLight2 = new THREE.PointLight(0x404040, 1, 100);
-        pointLight2.position.set(0, -15, 0);
+        pointLight2.position.set(0, - GLOBE_RADIUS * 2.5, 0);
 
         scene.add(pointLight);
         scene.add(pointLight2);
@@ -40,28 +48,96 @@ function GlobeViz() {
         const oceanMaterial = new THREE.MeshPhongMaterial({
             color: 0x1E90FF,
         });
-        const oceanSphereGeometry = new THREE.SphereGeometry(6, 360, 180);
+        const oceanSphereGeometry = new THREE.SphereGeometry(GLOBE_RADIUS, 360, 180);
         const oceanSphere = new THREE.Mesh(oceanSphereGeometry, oceanMaterial);
         scene.add(oceanSphere);
+    }
+
+    function addThreeGlobe(scene) {
+        // globe image texture: https://commons.wikimedia.org/wiki/File:Earthmap1000x500.jpg
+        // bump map: https://unpkg.com/three-globe@2.3.6/example/img/earth-topology.png
+        const globe = new ThreeGlobe({ animateIn: false })
+            .globeImageUrl('Earthmap1000x500.jpg')
+            .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png');
+        globe.scale.set(GLOBE_RADIUS * 0.01, GLOBE_RADIUS * 0.01, GLOBE_RADIUS * 0.01);
+        scene.add(globe);
     }
 
     useEffect(() => {
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = GLOBE_RADIUS * 3;
+
         const renderer = new THREE.WebGLRenderer();
         renderer.setClearColor(new THREE.Color(0x000000));
-        const orbitControls = new OrbitControls(camera, renderer.domElement);
-        orbitControls.minDistance = 9.0;
-        orbitControls.maxDistance = 20.0;
-        orbitControls.addEventListener('change', () => renderer.render(scene, camera));
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
 
-        addOceanSphere(scene);
+        const orbitControls = createOrbitControls(camera, renderer.domElement);
+        orbitControls.addEventListener('change', () => renderer.render(scene, camera));
+
+        addThreeGlobe(scene);
+        // addOceanSphere(scene);
         loadLights(scene);
 
-        camera.position.z = 20;
         renderer.render(scene, camera);
+
+        function getTreeDensityBlockMaterial() {
+            return new THREE.ShaderMaterial({
+                uniforms: {
+                    groundRadius: { value: 6.0 },
+                    groundColor: new THREE.Uniform(new THREE.Color(0x6B8E23)),
+                    skyColor: new THREE.Uniform(new THREE.Color(0x00ff00)),
+                },
+                vertexShader: `
+                    varying vec3 globalPosition;
+                    void main() {
+                      globalPosition = position;
+                      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+                    }
+                `,
+                fragmentShader: `
+                    varying vec3 globalPosition;
+                    uniform float groundRadius;
+                    uniform vec3 groundColor;
+                    uniform vec3 skyColor;
+
+                    void main() {
+                        float distToOrigin = distance(globalPosition, vec3(0, 0, 0));
+                        gl_FragColor = vec4(mix(groundColor, skyColor, distToOrigin - groundRadius), 1.0);
+                    }
+                `,
+            });
+        }
+
+        function getBlockVertices(normalizedValue, minLat, minLong, maxLat, maxLong) {
+            const vertices = [];
+            const phi = minLong * Math.PI / 180.0;
+            const theta = minLat * Math.PI / 180.0;
+            const maxPhi = maxLong * Math.PI / 180.0;
+            const maxTheta = maxLat * Math.PI / 180.0;
+
+            const coords = [
+                sphericalCoordsToVector3(GLOBE_RADIUS, phi, theta),
+                sphericalCoordsToVector3(GLOBE_RADIUS, phi, maxTheta),
+                sphericalCoordsToVector3(GLOBE_RADIUS, maxPhi, theta),
+                sphericalCoordsToVector3(GLOBE_RADIUS, maxPhi, maxTheta),
+                sphericalCoordsToVector3(GLOBE_RADIUS + normalizedValue, phi, theta),
+                sphericalCoordsToVector3(GLOBE_RADIUS + normalizedValue, phi, maxTheta),
+                sphericalCoordsToVector3(GLOBE_RADIUS + normalizedValue, maxPhi, theta),
+                sphericalCoordsToVector3(GLOBE_RADIUS + normalizedValue, maxPhi, maxTheta),
+            ];
+            const indices = [
+                0, 1, 2, 2, 1, 3, 0, 2, 4, 4, 2, 6, 2, 3, 6, 6, 3, 7, 3, 1, 7, 7, 1, 5, 1, 0, 5, 5, 0, 4, 4, 5, 6, 6, 5, 7
+            ];
+            indices.forEach(index => {
+                const arr = coords[index].toArray();
+                vertices.push(arr[0]);
+                vertices.push(arr[1]);
+                vertices.push(arr[2]);
+            });
+            return vertices;
+        }
 
         // === THREE.JS CODE START ===
         GeoTIFF.fromUrl('data/Crowther_Nature_Biome_Revision_01_WGS84_GeoTiff_downsampled.tif')
@@ -75,31 +151,7 @@ function GlobeViz() {
                 const rasterLatitudeFactor = (maxLatitude - minLatitude) / height;
                 let maxVal = 0;
 
-                const material = new THREE.ShaderMaterial({
-                    uniforms: {
-                        groundRadius: { value: 6.0 },
-                        groundColor: new THREE.Uniform(new THREE.Color(0x6B8E23)),
-                        skyColor: new THREE.Uniform(new THREE.Color(0x00ff00)),
-                    },
-                    vertexShader: `
-                        varying vec3 globalPosition;
-                        void main() {
-                          globalPosition = position;
-                          gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-                        }
-                    `,
-                    fragmentShader: `
-                        varying vec3 globalPosition;
-                        uniform float groundRadius;
-                        uniform vec3 groundColor;
-                        uniform vec3 skyColor;
-
-                        void main() {
-                            float distToOrigin = distance(globalPosition, vec3(0, 0, 0));
-                            gl_FragColor = vec4(mix(groundColor, skyColor, distToOrigin - groundRadius), 1.0);
-                        }
-                    `,
-                });
+                const material = getTreeDensityBlockMaterial();
 
                 image.readRasters().then(raster => {
 
@@ -120,32 +172,12 @@ function GlobeViz() {
 
                             const longitude = c * rasterLongitudeFactor + minLongitude;
                             const nextLongitude = (c + step) * rasterLongitudeFactor + minLongitude;
-                            const latitude = r * rasterLatitudeFactor + minLatitude;
-                            const nextLatitude = (r + step) * rasterLatitudeFactor + minLatitude;
+                            const latitude = maxLatitude - r * rasterLatitudeFactor;
+                            const nextLatitude = maxLatitude - (r + step) * rasterLatitudeFactor;
 
-                            const phi = longitude * Math.PI / 180.0;
-                            const theta = latitude * Math.PI / 180.0;
-                            const maxPhi = nextLongitude * Math.PI / 180.0;
-                            const maxTheta = nextLatitude * Math.PI / 180.0;
-
-                            const coords = [
-                                sphericalCoordsToVector3(6, phi, theta),
-                                sphericalCoordsToVector3(6, phi, maxTheta),
-                                sphericalCoordsToVector3(6, maxPhi, theta),
-                                sphericalCoordsToVector3(6, maxPhi, maxTheta),
-                                sphericalCoordsToVector3(6 + normalizedValue, phi, theta),
-                                sphericalCoordsToVector3(6 + normalizedValue, phi, maxTheta),
-                                sphericalCoordsToVector3(6 + normalizedValue, maxPhi, theta),
-                                sphericalCoordsToVector3(6 + normalizedValue, maxPhi, maxTheta),
-                            ];
-                            const indices = [
-                                0, 1, 2, 2, 1, 3, 0, 2, 4, 4, 2, 6, 2, 3, 6, 6, 3, 7, 3, 1, 7, 7, 1, 5, 1, 0, 5, 5, 0, 4, 4, 5, 6, 6, 5, 7
-                            ];
-                            indices.forEach(index => {
-                                const arr = coords[index].toArray();
-                                allBlockVertices.push(arr[0]);
-                                allBlockVertices.push(arr[1]);
-                                allBlockVertices.push(arr[2]);
+                            const blockVertices = getBlockVertices(normalizedValue, latitude, longitude, nextLatitude, nextLongitude);
+                            blockVertices.forEach(coord => {
+                                allBlockVertices.push(coord);
                             });
                         }
                     }
